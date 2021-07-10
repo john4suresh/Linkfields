92fa64c254cf1ef29d38c5a6dd8e1e501d05dc8b
john4suresh <johnsuresh777@gmail.com>
2021-07-10 12:52:53 +0530

    Added Pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,53 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { fetchData } from "../redux/DataAction";
-import { Table } from "antd";
+import { Pagination } from "antd";
 import { Link } from "react-router-dom";
 
 const Home = (props) => {
-  const data = props.data;
-  console.log(data);
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      render: (text, record, index) => {
-        return <Link to={`/${index + 1}`}>{text}</Link>;
-      },
-    },
-  ];
+  const { data, fetchData } = props;
+
+  const PokemonData = data.map((ele, index) => {
+    return { ...ele, id: index + 1 };
+  });
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
   useEffect(() => {
-    props.fetchData();
+    fetchData();
   }, []);
+
+  const onChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  function capitalize(word) {
+    const lower = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lower.slice(1);
+  }
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = PokemonData.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
-    <div>
-      <Table columns={columns} dataSource={data} />
+    <div className="home">
+      {currentPosts.map(({ name, id }) => (
+        <h1 key={id} className="listItem">
+          <Link className="link" to={`/${id}`}>
+            {capitalize(name)}
+          </Link>
+        </h1>
+      ))}
+      <Pagination
+        current={currentPage}
+        total={data.length}
+        onChange={onChange}
+        showSizeChanger={false}
+      />
     </div>
   );
 };
