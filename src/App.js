b3e738894931cf3285cce64fff82f44b41cfa26e
john4suresh <johnsuresh777@gmail.com>
2021-07-10 12:52:10 +0530

    Added Details Page to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
-import Basic from "./components/Basic";
 import { Route } from "react-router-dom";
+import DetailPokemon from "./components/DetailPokemon";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Home} />
       <Route exact path="/:id">
-        <Basic />
+        <DetailPokemon />
       </Route>
     </div>
   );
